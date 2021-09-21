6a7f2cb13c787adee77079a750925c43ae3501af
ali-sorkhi <ali.sorkhi92@gmail.com>
2021-09-21 12:41:52 +0430

    nameChanging

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import HomePage from "./Pages/Home";
+import HomePage from "./Pages/HomePage";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
